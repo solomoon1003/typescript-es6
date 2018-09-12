bdc6d5c3689fb712ce5090ac8aaf6f2bf2b06073
Andy <solomoon1003@gmail.com>
2018-09-12 23:19:17 +0700

    ok

--- lesson11.js
+++ lesson11.js
@@ -6,11 +6,18 @@ var displayColor = function () {
         color[_i] = arguments[_i];
     }
     // });
-    for (var i in color) {
-        console.log(color[i]);
+    for (var i_1 in color) {
+        console.log(color[i_1]);
     }
 };
 displayColor('Red', 'Blue', 'Green');
 var em = ['a', 'b', 'c'];
 var e1 = em[0], e2 = em.slice(1);
 console.log(e1);
+var em2 = {
+    id: 1,
+    name: 'fuck',
+    age: 100
+};
+var i = em2.id, ab = em2.age;
+console.log(ab);
--- lesson11.ts
+++ lesson11.ts
@@ -16,3 +16,16 @@ displayColor('Red', 'Blue', 'Green');
 var em = ['a', 'b', 'c'];
 let [e1, ...e2] = em;
 console.log(e1);
+
+
+var em2 = {
+    id: 1,
+    name: 'fuck',
+    age: 100
+};
+
+
+var {  id : i, age: ab  } = em2;
+
+console.log(ab);
+
